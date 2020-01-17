8a6244a3280681598a20afb3adaa8dc9044c5986
root <hizhouqiong@163.com>
2020-01-17 17:27:35 +0800

    base

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,22 @@
 set -e
 
 # 生成静态文件
-yarn build
+# yarn build
 
 # 进入生成的文件夹
-cd ./dist
+# cd ./dist
 
 # git init
-git add -A
-git commit -m 'deploy'
+# git add -A
+# git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hizhouqiong/publish_phone.git master
+git push -f git@github.com:hizhouqiong/vue_phone_demo.git master:views
 # 链接远程仓库
-git remote add origin https://github.com/hizhouqiong/publish_phone.git
+git remote add origin https://github.com/hizhouqiong/vue_phone_demo.git
 # 拉取远程仓库的文件
 git pull --rebase origin master  
 # 同步更新代码
 git push -u origin master
 
-cd -
+# cd -
